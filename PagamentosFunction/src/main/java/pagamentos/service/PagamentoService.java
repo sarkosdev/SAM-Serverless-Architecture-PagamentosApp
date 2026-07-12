4ad1ac28cd0015fbcffa193013057f23478ed15c
sarkosdev <142413026+sarkosdev@users.noreply.github.com>
2026-07-12 21:02:14 +0100

    fix dynamodb struct

--- PagamentosFunction/src/main/java/pagamentos/service/PagamentoService.java
+++ PagamentosFunction/src/main/java/pagamentos/service/PagamentoService.java
@@ -213,8 +213,6 @@ public class PagamentoService {
         // Complete Map that will be saved in our Table
         Map<String, AttributeValue> item = new HashMap<>();
         item.put("id", AttributeValue.builder().s(pagamentoId).build());
-        item.put("PK", AttributeValue.builder().s("USER#" + userName).build());
-        item.put("SK", AttributeValue.builder().s("PAGAMENTO#" + createdAt + "#" + pagamentoId).build());
         item.put("type", AttributeValue.builder().s("PAGAMENTO").build());
         item.put("userName", AttributeValue.builder().s(userName).build());
         item.put("createdAt", AttributeValue.builder().s(createdAt).build());
